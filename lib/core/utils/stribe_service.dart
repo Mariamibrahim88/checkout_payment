0af1dcd100efe4ba1acaa922a46084e95abcc34c
maria <mariamibrahimmi4142726@gmail.com>
2024-09-04 02:30:05 +0300

    create api service

---
+++ lib/core/utils/stribe_service.dart
@@ -0,0 +1,7 @@
+import 'package:checkout_payment_ui/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment_ui/features/checkout/data/models/payment_intern_model/payment_intern_model.dart';
+
+class StribeService {
+  // PaymentInternModel createpaymentIntern(
+  //     PaymentIntentInputModel paymentInternInputModel) {}
+}
