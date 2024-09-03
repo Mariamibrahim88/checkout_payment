06356ded7b0d643437bbc0c9496d44a564c2d56d
maria <mariamibrahimmi4142726@gmail.com>
2024-09-03 23:25:27 +0300

    add flutter stribe package

--- android/app/src/main/kotlin/com/example/checkout_payment_ui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/checkout_payment_ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.checkout_payment_ui
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
+
+
