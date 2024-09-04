756be96305efa6133716c1f74262b3de6a141751
maria <mariamibrahimmi4142726@gmail.com>
2024-09-04 21:08:21 +0300

    implement payment intent

--- lib/core/utils/stribe_service.dart
+++ lib/core/utils/stribe_service.dart
@@ -1,7 +1,20 @@
+import 'package:checkout_payment_ui/core/utils/api_keys.dart';
+import 'package:checkout_payment_ui/core/utils/api_service.dart';
 import 'package:checkout_payment_ui/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:checkout_payment_ui/features/checkout/data/models/payment_intern_model/payment_intern_model.dart';
 
 class StribeService {
-  // PaymentInternModel createpaymentIntern(
-  //     PaymentIntentInputModel paymentInternInputModel) {}
+  final ApiService apiService = ApiService();
+  Future<PaymentInternModel> createpaymentIntern(
+      PaymentIntentInputModel paymentInternInputModel) async {
+    var response = await apiService.post(
+      url: 'https://api.stripe.com/v1/payment_intents',
+      body: paymentInternInputModel.toJson(),
+      token: ApiKeys.secretKey,
+    );
+
+    var result = PaymentInternModel.fromJson(response.data);
+
+    return result;
+  }
 }
